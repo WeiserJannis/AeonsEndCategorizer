52e2b583dba0054443333b165c1df11f72e66361
Jannis Weiser <jannis.weiser@gmail.com>
2020-08-09 10:14:37 +0200

    read resources/test.jpg and output the text

--- src/main/app/__init__.py
+++ src/main/app/__init__.py
@@ -1,11 +1,16 @@
+import pathlib
+
 from kivy.app import App
 from kivy.uix.label import Label
-
+from PIL import Image
+import pytesseract
 
 class MainApp(App):
     def build(self):
 
-        label = Label(text='Hallooo Natascha',
+        imagePath = pathlib.Path(__file__).parent.parent / 'resources' / 'test.jpg'
+
+        label = Label(text=pytesseract.image_to_string(Image.open(imagePath), lang='eng'),
                       size_hint=(.5, .5),
                       pos_hint={'center_x': .5, 'center_y': .5})
         return label
